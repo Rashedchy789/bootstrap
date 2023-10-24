996ad83d95f7684fa3e66d461520048f904df271
RashedChy <136729495+Rashedchy789@users.noreply.github.com>
2023-10-24 12:53:24 +0600

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -2,5 +2,15 @@ $(function () {
   // SLICK SLIDER
   $('#banner').slick({
     arrows: false,
+    dots: true,
+    fade: true,
+    dotsClass: 'bannerDots container',
   });
+  // BOOTSTRAP TOOLTIP
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
 });
