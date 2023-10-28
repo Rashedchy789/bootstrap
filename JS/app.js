49c741931c172f1676499862479182090d6b992e
RashedChy <136729495+Rashedchy789@users.noreply.github.com>
2023-10-28 19:05:10 +0600

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -13,4 +13,11 @@ $(function () {
   const tooltipList = [...tooltipTriggerList].map(
     tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)
   );
+
+  $('.timing').countdown('2023/11/12', function (event) {
+    $('.days').text(event.strftime('%D'));
+    $('.hour').text(event.strftime('%H'));
+    $('.mins').text(event.strftime('%M'));
+    $('.sec').text(event.strftime('%S'));
+  });
 });
